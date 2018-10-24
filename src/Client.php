cb9f50c11006de0bee6aeb9d3c719d526fa2d1d6
William Cabrera <cabrerawilliam@gmail.com>
2018-10-24 03:24:13 -0400

    Create method request

--- src/Client.php
+++ src/Client.php
@@ -17,4 +17,42 @@ class Client
         if (isset($params['password'])) $this->password = $params['password'];
     }
 
+    private function request($params)
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => $this->host.':'.$this->port.'/transmission/rpc',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_CUSTOMREQUEST => "POST",
+          CURLOPT_POSTFIELDS => json_encode($params),
+          CURLOPT_HTTPHEADER => array(
+            (isset($this->auth) ? $this->auth : ''),
+            "Content-Type: application/json",
+            "X-Transmission-Session-Id: " . $this->sessionId
+          ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+
+        switch($httpCode) {
+            case '200':
+                return $response;
+            case '409':
+                preg_match_all('/X-Transmission-Session-Id: ([^<]*)/m', $response, $m, PREG_SET_ORDER, 0);
+                $this->sessionId = $m[0][1];
+                return json_decode($this->request($params));
+            case '401':
+                return [
+                    'arguments' => [],
+                    'result' => '401: Unauthorized User'
+                ];
+            default:
+                return [
+                    'arguments' => [],
+                    'result' => $httpCode . ': Unexpected response received from Transmission'
+                ];
+        }
+    }
 }
