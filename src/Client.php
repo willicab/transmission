9cb71b442a4923b7ad49990b40acdbe54160cf68
William Cabrera <cabrerawilliam@gmail.com>
2018-10-24 03:23:35 -0400

    Create method construct

--- src/Client.php
+++ src/Client.php
@@ -4,5 +4,17 @@ namespace Willicab\Transmission;
 
 class Client
 {
+    private $host = '';
+    private $port = 9091;
+    private $user;
+    private $password;
+
+    public function __construct($params = [])
+    {
+        $this->host = isset($params['host']) ? $params['host'] : 'http://localhost';
+        $this->port = isset($params['port']) ? $params['port'] : 9091 ;
+        if (isset($params['user'])) $this->user = $params['user'];
+        if (isset($params['password'])) $this->password = $params['password'];
+    }
 
 }
